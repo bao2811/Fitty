551657616acdf0375f753b53486039209ef482b2
TVQuyet05 <tranvanquyet09112005@gmail.com>
2026-04-24 07:25:53 +0700

    fix: fix library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.fitty"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.fitty"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
